3038ee8728a6dc9b3b6305d1756197188f974c23
Your Name <68496998+nkieve@users.noreply.github.com>
2026-06-12 12:34:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -721,7 +721,7 @@ class NovelScene {
         ctx.textBaseline = 'top';
 
         const textX = textBoxX + padding + textBoxWidth * 0.08;
-        const textY = textBoxY + padding + (isMobile ? 0 : 20);
+        const textY = textBoxY + padding + (isMobile ? 0 : 20) + textBoxHeight * 0.20;
         const maxWidth = textBoxWidth - 2 * padding;
 
 
